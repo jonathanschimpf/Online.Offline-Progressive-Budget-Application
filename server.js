49ab0993d2b19710858f5741ca1b14c5cf34cf7b
Jawnathan <jonathan@jonathanschimpf.com>
2020-10-25 18:10:27 -0400

    adding process.env.MONGODB_URI for heroku deployment

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+
+// MongoDB locally + process.env setup for Heroku deployment.
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
