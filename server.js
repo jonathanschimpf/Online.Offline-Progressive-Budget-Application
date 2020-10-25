8ddd390bb54e6c37a6eb5eeaa26829b86ec73b00
Jawnathan <jonathan@jonathanschimpf.com>
2020-10-25 18:12:25 -0400

    adding process.env.PORT ||

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+
+// process.env.PORT or 3000
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
